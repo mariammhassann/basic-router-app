e300b24f8fae6a733273851f4562d2130c3af159
MAriam Ashraf <mariamashrafhassan25.5@gmail.com>
2025-08-31 17:40:51 +0300

    Add React simple Router with Home and About pages

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+ import React from 'react';
+ 
+ const About = () => (
+  <div>
+    <h2>About Page</h2>
+    <p>This page provides information about our basic app.</p>
+  </div>
+ );
+ export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h2>Home Page</h2>
+    <p>Welcome to the simplest routing example!</p>
+  </div>
+);
+
+export default Home;
